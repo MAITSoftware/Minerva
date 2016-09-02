0b3933c24435f1dde2506e22469c1f7378e4273b
Ignacio Rodríguez <ignacio@sugarlabs.org>
2016-09-02 04:44:54 -0300

    Inicio diseño interfaz -- Viernes 04:44am

--- Codigo/Minerva/frmAdminDocentes.vb
+++ Codigo/Minerva/frmAdminDocentes.vb
@@ -1,3 +1,11 @@
 ﻿Public Class frmAdminDocentes
     ' Clase principal para la administracion de docentes
+
+    Private Sub frmAdminDocentes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call New ToolTip().SetToolTip(lblObligatorio1, "Dato obligatorio")
+        Call New ToolTip().SetToolTip(lblObligatorio2, "Dato obligatorio")
+        Call New ToolTip().SetToolTip(lblObligatorio3, "Dato obligatorio")
+        Call New ToolTip().SetToolTip(lblObligatorio4, "Dato obligatorio")
+        Call New ToolTip().SetToolTip(lblObligatorio5, "Dato obligatorio")
+    End Sub
 End Class
