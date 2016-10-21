ea1f64bbb44b38c65f4ba452276868faadda2c65
Ignacio Rodríguez <ignacio@sugarlabs.org>
2016-10-21 00:03:47 -0300

    Mapa de código

--- Codigo/Minerva/frmAdminHorarios.vb
+++ Codigo/Minerva/frmAdminHorarios.vb
@@ -1,4 +1,13 @@
 ﻿Public Class frmAdminHorarios
     ' Clase principal para la administración de horarios
+    Public Sub New()
+
+        ' Llamada necesaria para el diseñador.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        Dim x As BaseDeDatos
+        x = New BaseDeDatos()
+    End Sub
 
 End Class
