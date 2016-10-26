b89945bd2d5bec2dc4f86452b796ff01511811c0
Ignacio Rodríguez <ignacio@sugarlabs.org>
2016-10-26 06:06:36 -0300

    ex

--- Codigo/Minerva/Bedelia/Resources.Designer.vb
+++ Codigo/Minerva/Bedelia/Resources.Designer.vb
@@ -63,16 +63,6 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property _default() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("default", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        '''</summary>
         Friend ReadOnly Property agregar_hover() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("agregar_hover", resourceCulture)
@@ -126,16 +116,6 @@ Namespace My.Resources
         Friend ReadOnly Property cancelar_normal() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("cancelar_normal", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property default__1_() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("default (1)", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
